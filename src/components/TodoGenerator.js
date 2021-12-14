8da367b8b621b633ce6a625c8a8a43ae531524bb
Julia Ng <ngju@oocl.com>
2021-12-14 09:49:45 +0800

    [ReactTodos][JuliaNg] - feat: refine the CSS

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -4,7 +4,7 @@ function TodoGenerator(props) {
     const [todo, setTodo] = useState("");
 
     function addTodo() {
-        if (document.getElementById("newTodo").value != null) {
+        if (document.getElementById("newTodo").value.trim() != "") {
             props.updateList(document.getElementById("newTodo").value);
             document.getElementById("newTodo").value = "";
         }

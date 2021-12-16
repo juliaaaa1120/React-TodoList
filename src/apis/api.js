c9fca4807a22fb7da1bb493e22218d5ad0487eef
Julia Ng <ngju@oocl.com>
2021-12-16 18:11:26 +0800

    [SpringBootTodos][JuliaNg] - feat: front and back end integration

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://61b9880c38f69a0017ce60a3.mockapi.io"
+    baseURL: "http://localhost:8080"
 });
 
 export default api;
